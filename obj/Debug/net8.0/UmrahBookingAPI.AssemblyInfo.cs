43e854fac46c29e65c4978d892f7b8c3de77d6c4
kurd12 <43173514+kurd12@users.noreply.github.com>
2025-08-12 01:37:46 +0300

    Fix Dockerfile name

--- obj/Debug/net8.0/UmrahBookingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/UmrahBookingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UmrahBookingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f05b1818c3d7f893c22fa575869e9d00443d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UmrahBookingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UmrahBookingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
