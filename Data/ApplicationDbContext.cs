a4ff2465e7a07f573fa326759b0f8cfff2c8efc7
kurd12 <43173514+kurd12@users.noreply.github.com>
2025-08-13 01:18:04 +0300

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,18 @@
 ﻿// Faili: Data/ApplicationDbContext.cs
-using Microsoft.EntityFrameworkCore;
 
+// usingـە پێویستەکان
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Linq;
+
+// پێناسەی DbContext
 public class ApplicationDbContext : DbContext
 {
+    // Constructor
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+    // پێناسەی خشتەکان (Tables)
     public DbSet<User> Users { get; set; }
     public DbSet<Trip> Trips { get; set; }
     public DbSet<Booking> Bookings { get; set; }
@@ -13,49 +21,58 @@ public class ApplicationDbContext : DbContext
     public DbSet<Payment> Payments { get; set; }
     public DbSet<TripItinerary> TripItineraries { get; set; }
 
-    // ================== چارەسەری یەکلاکەرەوە لێرەدایە ==================
-    // Faili: Data/ApplicationDbContext.cs
-    // ... (کۆدی تر)
-
-    // Faili: Data/ApplicationDbContext.cs
-    // ... (کۆدی تر)
-
+    // پێناسەکردنی مۆدێل و پەیوەندییەکان
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
+        // ================== چارەسەری ١: پێناسەکردنی پەیوەندییەکان ==================
+        // ئەمە کێشەی Build چارەسەر دەکات
         modelBuilder.Entity<Booking>(entity =>
         {
-            // ================== چارەسەری زۆر سادە لێرەدایە ==================
             // پەیوەندی لەگەڵ User
             entity.HasOne(b => b.User)
-                  .WithMany() // <-- ناوەڕۆکی کەوانەکە لابرا
+                  .WithMany() // EF Core خۆی پەیوەندییەکە تێدەگات
                   .HasForeignKey(b => b.UserID)
                   .IsRequired();
 
             // پەیوەندی لەگەڵ Trip
             entity.HasOne(b => b.Trip)
-                  .WithMany() // <-- ناوەڕۆکی کەوانەکە لابرا
+                  .WithMany()
                   .HasForeignKey(b => b.TripID)
                   .IsRequired();
 
             // پەیوەندی لەگەڵ Representative
             entity.HasOne(b => b.Representative)
-                  .WithMany() // <-- ناوەڕۆکی کەوانەکە لابرا
+                  .WithMany()
                   .HasForeignKey(b => b.RepID)
-                  .IsRequired(false);
+                  .IsRequired(false); // ئارەزوومەندانە
 
             // پەیوەندی لەگەڵ CampaignLeader
             entity.HasOne(b => b.CampaignLeader)
-                  .WithMany() // <-- ناوەڕۆکی کەوانەکە لابرا
+                  .WithMany()
                   .HasForeignKey(b => b.LeaderID)
-                  .IsRequired(false);
-            // =======================================================
+                  .IsRequired(false); // ئارەزوومەندانە
         });
-    }
-    // ... (کۆدی تر)
-    // ... (کۆدی تر)
-    // dotnet build
 
-    // =================================================================
+        // ================== چارەسەری ٢: چارەسەری کێشەی Enum ==================
+        // ئەمە کێشەی 'Can't convert Enum to Int32' چارەسەر دەکات
+        // ئەم کۆدە بە شێوەیەکی ئۆتۆماتیکی هەموو Enumـەکان دەکات بە string
+        if (Database.ProviderName == "Pomelo.EntityFrameworkCore.MySql")
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType.IsEnum)
+                    {
+                        var enumType = property.ClrType;
+                        var converterType = typeof(EnumToStringConverter<>).MakeGenericType(enumType);
+                        var converter = (ValueConverter)Activator.CreateInstance(converterType, null);
+                        property.SetValueConverter(converter);
+                    }
+                }
+            }
+        }
+    }
 }
