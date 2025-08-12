5c13ab9c3c2b31066a2d59417d28db97919b1906
kurd12 <43173514+kurd12@users.noreply.github.com>
2025-08-12 03:46:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore; // ئەمە زیاد بکە لە سەرەوە
+﻿using Microsoft.EntityFrameworkCore;
+using UmrahBookingAPI; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// کۆدی نوێ و دروست بۆ پاکێجی Pomelo
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// **کۆدی نوێ و زیرەک بۆ دروستکردنی Connection String**
+// ئەگەر لەسەر سێرڤەری Railway بووین، خۆی Connection String دروست دەکات
+if (builder.Environment.IsProduction())
+{
+    var dbHost = builder.Configuration["MYSQLHOST"];
+    var dbPort = builder.Configuration["MYSQLPORT"];
+    var dbUser = builder.Configuration["MYSQLUSER"];
+    var dbPass = builder.Configuration["MYSQLPASSWORD"];
+    var dbName = builder.Configuration["MYSQLDATABASE"];
+
+    connectionString = $"Server={dbHost};Port={dbPort};Database={dbName};User={dbUser};Password={dbPass};";
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,14 +31,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
+// if (app.Environment.IsDevelopment()) // ئەم بەشەمان لابرد بۆ ئەوەی Swagger هەمیشە کار بکات
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
-
-app.UseMiddleware<ApiKeyMiddleware>();
 
 app.UseAuthorization();
 
